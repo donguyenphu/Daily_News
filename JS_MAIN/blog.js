2c7282ed5fbf8eda5c079140b4bccc4d0cb827be
donguyenphu <donguyenphu022008@gmail.com>
2024-07-26 16:48:49 +0700

    update

--- JS_MAIN/blog.js
+++ JS_MAIN/blog.js
@@ -1,166 +1,159 @@
-// const API = {
-//     call: function() {
-//         return axios.create({
-//             baseURL: 'https://apiforlearning.zendvn.com/api/v2/',
-//         });
-//     },
-//     callWithToken: function(token) {
-//         if (!token) {
-//             token = localStorage.getItem('ACCESS_TOKEN');
-//         }
-//         return axios.create({
-//             baseURL: 'https://apiforlearning.zendvn.com/api/v2/',
-//             headers: {
-//                 Authorization: `Bearer ${token}`,
-//             },
-//         });
-//     }, 
-// };
-
 dayjs.extend(window.dayjs_plugin_relativeTime);
 dayjs.locale('vi');
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let id = urlParams.get('id');
+let articlesMain = document.getElementById('articlesMain');
+let myPagination = document.getElementById('myPagination');
+let overCategoryName = document.getElementById('overCategoryName');
+let FullRecentPosts = document.getElementById('FullRecentPosts');
+let RecentTitle = document.getElementById('RecentTitle');
+let RecentWrapper = document.getElementById('RecentWrapper');
 
-const queryString=window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id=urlParams.get('id');
-const articlesMain=document.getElementById('articlesMain');
-const myPagination=document.getElementById('myPagination');
-const overCategoryName=document.getElementById('overCategoryName');
-// RENDER MENUS
 
-// let first=parseInt(urlParams.get('page')); ///////
-let first=parseInt(urlParams.get('page'));
+let first = parseInt(urlParams.get('page'));
 if (isNaN(first) === true) {
-    // neo 
-    // console.log('FALSE:::::');
-    first=1;   
+    first = 1;
 }
 if (isNaN(parseInt(id))) {
-    window.location.href="index.html";
+    window.location.href = "index.html";
 }
 
-console.log('PAGESSSSSSSS:',first,'AND:',id);
+console.log('PAGESSSSSSSS:', first, 'AND:', id);
 
 
 getArticles(first);
 
+let html =
+    `
+    <div class="widget-title mb-30" id="RecentTitle">
+        <h6 class="title">Tin gần đây</h6>
+        <div class="section-title-line"></div>
+    </div>
+`;
+API.call().get('articles/popular?limit=3').then(function (res) {
+    const articles = res.data.data;
+    articles.forEach((item, index) => {
+        if (index === 0) {
+            html +=
+                /* html */
+                `   
+            <div class="hot-post-item">
+                <div class="hot-post-thumb">
+                    <a href="detail.html"><img src="${item.thumb}" alt=""></a>
+                </div>
+                <div class="hot-post-content">
+                    <a href="blog.html" class="post-tag">${item.category.name}</a>
+                    <h4 class="post-title"><a href="detail.html">${item.title}</a></h4>
+                    <div class="blog-post-meta">
+                        <ul class="list-wrap">
+                            <li><i class="flaticon-calendar"></i>${item.publish_date}</li>
+                            <li><i class="flaticon-history"></i>${dayjs(item.publish_date).fromNow()}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+        else {
+            html +=
+                /* html */
+                `
+            <div class="hot-post-item">
+                <div class="hot-post-content">
+                    <a href="blog.html" class="post-tag">${item.category.name}</a>
+                    <h4 class="post-title"><a href="detail.html">${item.title}</a></h4>
+                    <div class="blog-post-meta">
+                        <ul class="list-wrap">
+                            <li><i class="flaticon-calendar"></i>${item.publish_date}</li>
+                            <li><i class="flaticon-history"></i>${dayjs(item.publish_date).fromNow()}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+    });
+    FullRecentPosts.innerHTML = html;
+});
 
-API.call().get('categories_news').then(function(res) {
-    console.log('FOR NOW: ',res);
-    const articles=res.data.data;
+API.call().get('categories_news').then(function (res) {
+    console.log('FOR NOW: ', res);
+    const articles = res.data.data;
     console.log(articles);
-    let html='';
-    let html2=`<li class="menu-item-has-children"><a href="#">Danh mục khác</a>
+    let html = '';
+    let html2 = `<li class="menu-item-has-children"><a href="#">Danh mục khác</a>
                     <ul class="sub-menu">`;
-    articles.forEach((item,index) => {
-       if (index < 3) {
-            html+=
-            /* html */
-            `<li><a href="#">${item.name}</a></li>`;
+    articles.forEach((item, index) => {
+        if (index < 3) {
+            html +=
+                /* html */
+                `<li><a href="#">${item.name}</a></li>`;
         }
         else {
             /* html */
-            html2+=`<li><a href="#">${item.name}</a></li>`
+            html2 += `<li><a href="#">${item.name}</a></li>`
         }
     });
-    html2+=`</ul>
+    html2 += `</ul>
         </li>`;
-    menuFull.innerHTML=html+html2;
+    menuFull.innerHTML = html + html2;
 });
 
-/// RENDER CATEGORIES
-
-// API.call().get(`categories_news/${id}/articles?limit=5&page=${first}`).then(res => {
-//     const articles=res.data.data;
-//     console.log('ARTICLESSSSS:',articles);
-//     let html='';
-//     let totalPages=res.data.meta.total;
-//     /// for render pagination()
-//     renderPagination(totalPages);
-//     articles.forEach(item => {
-//         /* html */
-//         html+=
-//         `   
-//             <div class="weekly-post-item weekly-post-four">
-//                 <div class="weekly-post-thumb">
-//                     <a href="detail.html"><img src="${item.thumb}" alt="${item.title}"></a>
-//                 </div>
-//                 <div class="weekly-post-content">
-//                     <a href="blog.html" class="post-tag">${item.category.name}</a>
-//                     <h2 class="post-title"><a href="detail.html">${item.title}</a></h2>
-//                     <div class="blog-post-meta">
-//                         <ul class="list-wrap">
-//                             <li><i class="flaticon-calendar"></i>${item.publish_date}</li>
-//                             <li><i class="flaticon-history"></i>${dayjs(item.publish_date).fromNow()}</li>
-//                         </ul>
-//                     </div>
-//                     <p>${item.description}</p>
-//                     <div class="view-all-btn">
-//                         <a href="detail.html" class="link-btn">Read More
-//                             <span class="svg-icon">
-//                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" fill="none">
-//                                     <path d="M1.07692 10L0 8.92308L7.38462 1.53846H0.769231V0H10V9.23077H8.46154V2.61538L1.07692 10Z" fill="currentColor" />
-//                                     <path d="M1.07692 10L0 8.92308L7.38462 1.53846H0.769231V0H10V9.23077H8.46154V2.61538L1.07692 10Z" fill="currentColor" />
-//                                 </svg>
-//                             </span>
-//                         </a>
-//                     </div>
-//                 </div>
-//             </div>
-//         `;
-//     });
-//     articlesMain.innerHTML=html;
-// });
-
-//// 
-
-myPagination.addEventListener('click', function(e) {
-    const el=e.target;
+myPagination.addEventListener('click', function (e) {
+    const el = e.target;
     if (el.classList.contains('page-item')) {
-        first=parseInt(el.innerText);
-        console.log('NEWWWW:::::::::::',first);
-        mergeAndPush(first);    
+        first = parseInt(el.innerText);
+        console.log('NEWWWW:::::::::::', first);
+        getArticles(first);
     }
-    else if (el.classList.contains('prev') === true) {
+    if (el.classList.contains('page-item-prev') === true) {
         first--;
-        console.log('prevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv:',first);
-        mergeAndPush(first); /// tao trang moi
+        console.log('prevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv:', first);
+        getArticles(first);
     }
-    else if (el.classList.contains('next') === true) {
+    if (el.classList.contains('page-item-next') === true) {
         first++;
-        console.log('nexxxxxxxxxxxxxxxxxxxx',first);
-        mergeAndPush(first);
+        console.log('nexxxxxxxxxxxxxxxxxxxx', first);
+        getArticles(first);
     }
 });
 
 function renderPagination(total) {
-    const disPrev=(first === 1  ? 'pointer-events-none' : '');
-    const disNex=(first === total ? 'pointer-events-none' : '');
-    let html=`<li class="prev ${disPrev}"><a class="page-link" href="#">Previous</a></li>`;
+    const disPrev = (first === 0 ? 'pointer-events-none' : '');
+    const disNex = (first === total+1 ? 'pointer-events-none' : '');
+    myPagination.innerHTML = '';
+    let html = `<a class="page-item-prev page-link ${disPrev}">Previous</a>`;
     for (let index = 1; index <= total; index++) {
-        let active=(index === first ? 'active pointer-events-none' : '');
-        if (index === first) {
-            mergeAndPush(first); /// tao trang moi
-        }
-        html+=`<li class="page-item ${active}"><a class="page-link" href="#">${index}</a></li>`;
+        let active = (index === first ? 'active pointer-events-none' : '');
+        // if (index === first) {
+        //     mergeAndPush(first); /// tao trang moi
+        // }
+        html += `<a class="page-item page-link ${active}">${index}</a>`;
     }
-    html+=`<li class="next ${disNex}"><a class="page-link" href="#">Next</a></li>`;
-    myPagination.innerHTML=html;
+    html += `<a class="page-item-next page-link ${disNex}">Next</a>`;
+    myPagination.innerHTML = html;
 }
 
 function getArticles(first) {
+    urlParams.set('page', first);
+    let newPageLink = window.location.pathname + "?" + urlParams.toString();
+    history.pushState(null, "", newPageLink);
+
     API.call().get(`categories_news/${id}/articles?limit=5&page=${first}`).then(res => {
-        const articles=res.data.data;
-        let html='';
-        let totalPages=res.data.meta.total;
-        let TitleAll='';
+        const articles = res.data.data;
+        let html = '';
+        let totalPages = res.data.meta.total;
+        console.log('TOTAL:::: ', totalPages);
+        let TitleAll = '';
+        renderPagination(totalPages);
         // let overCategoryName='';
         /// for render pagination()
-        renderPagination(totalPages);
+        // renderPagination(totalPages);
         articles.forEach((item) => {
-            TitleAll=item.category.name;
-            html+= /* html */
-            `
+            TitleAll = item.category.name;
+            html += /* html */
+                `
                 <div class="weekly-post-item weekly-post-four">
                     <div class="weekly-post-thumb">
                         <a href="detail.html"><img src="${item.thumb}" alt="${item.title}"></a>
@@ -190,19 +183,19 @@ function getArticles(first) {
             `;
         });
 
-        overCategoryName.innerText=TitleAll;
+        overCategoryName.innerText = TitleAll;
 
-        articlesMain.innerHTML=html;
+        articlesMain.innerHTML = html;
     })
-    .catch(function(err) {
-        window.location.href('index.html');
-    });
+        .catch(function (err) {
+            window.location.href('index.html');
+        });
 }
 
 function mergeAndPush(first) {
     urlParams.set('page', first);
-    let newPageLink=window.location.pathname + "?" +urlParams.toString();
-    history.pushState(null,"",newPageLink);
+    let newPageLink = window.location.pathname + "?" + urlParams.toString();
+    history.pushState(null, "", newPageLink);
     getArticles(first);
 }
 
--- JS_MAIN/general.js
+++ JS_MAIN/general.js
@@ -18,7 +18,6 @@ const API = {
 };
 
 const ACCESS_TOKEN='ACCESS_TOKEN';
-
 dayjs.extend(window.dayjs_plugin_relativeTime);
 dayjs.locale('vi');
 
@@ -41,5 +40,60 @@ function toastMessage(check) {
     duration: 3000,
     close: true,
     }).showToast();
+}
+
+function getRecentPosts(FullRecentPosts,RecentTitle,RecentWrapper) {
+    RecentTitle.innerHTML=
+    /* html */
+    `
+    <h6 class="title">Tin gần đây</h6>
+    <div class="section-title-line"></div>
+    `;
+    let html='';
+    API.call().get('articles/popular?limit=3').then(function(res) {
+        const articles=res.data.data;
+        articles.forEach((item,index) => {
+            if (index===0) {
+                html+=
+                /* html */
+                `   
+                <div class="hot-post-item">
+                    <div class="hot-post-thumb">
+                        <a href="detail.html"><img src="assets/img/blog/blog_rc_post.jpg" alt=""></a>
+                    </div>
+                    <div class="hot-post-content">
+                        <a href="blog.html" class="post-tag">${item.category.name}</a>
+                        <h4 class="post-title"><a href="detail.html">${item.title}</a></h4>
+                        <div class="blog-post-meta">
+                            <ul class="list-wrap">
+                                <li><i class="flaticon-calendar"></i>${item.publish_date}</li>
+                                <li><i class="flaticon-history"></i>${dayjs(item.publish_date).fromNow()}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }
+            else {
+                html+=
+                /* html */
+                `
+                <div class="hot-post-item">
+                    <div class="hot-post-content">
+                        <a href="blog.html" class="post-tag">${item.category.name}</a>
+                        <h4 class="post-title"><a href="detail.html">${item.title}</a></h4>
+                        <div class="blog-post-meta">
+                            <ul class="list-wrap">
+                                <li><i class="flaticon-calendar"></i>${item.publish_date}</li>
+                                <li><i class="flaticon-history"></i>${dayjs(item.publish_date).fromNow()}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                `;
+            }
+        });
+    });
+    RecentWrapper.innerHTML=html;
 }
 
