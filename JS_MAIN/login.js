c9bcadd811e58f33258d644959df2704178556bd
donguyenphu <donguyenphu022008@gmail.com>
2024-08-12 17:13:51 +0700

    no message

--- JS_MAIN/login.js
+++ JS_MAIN/login.js
@@ -3,7 +3,7 @@ API.callWithToken().get('/auth/me').then((res) => {
 });
 
 const NewsletterWrapper=document.getElementById('NewsletterWrapper');   
-const AuthForm=document.getElementById('AuthForm');
+
 const email=document.getElementById('email');
 const password=document.getElementById('password');
 const formMessage=document.getElementById('formMessage');
@@ -14,7 +14,7 @@ NewsletterWrapper.innerHTML=/* html */ `
         <h2 class="title">Cập nhật tin tức mới nhất, đúng nhất</h2>
     </div>
     <div class="newsletter-form">
-        <form action="#" id="auth-form">
+        <form action="#" id="AuthForm">
             <div class="form-grp">
                 <input type="email" placeholder="Email" name="email" id="email" required>
             </div>
@@ -40,6 +40,9 @@ GoogleMap.innerHTML= /* html */
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3726.8914518533043!2d106.67795077441353!3d20.916686380704796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7bfbc27bc233%3A0x89f213ed1023838c!2zSGFtIExvbmcgX3Ro4buLIHRy4bqlbiBuw7ppIMSRw6hvX2h1eeG7h24gdGjhu6d5IG5ndXnDqm5fdHAgaOG6o2kgcGjDsm5n!5e0!3m2!1svi!2s!4v1721462510445!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 `;
 
+
+const AuthForm=document.getElementById('AuthForm');
+
 AuthForm.addEventListener('submit', function (res) {
     res.preventDefault();
 
@@ -51,7 +54,7 @@ AuthForm.addEventListener('submit', function (res) {
         password:valp
     }
 
-    API.call('auth/login',data).then(function(res) {
+    API.call().post('auth/login',data).then(function(res) {
         localStorage.setItem('ACCESS_TOKEN',res.data.access_token);
         window.location.href='index.html';
     })
