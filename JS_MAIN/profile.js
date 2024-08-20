a81be7cabbcc4b178801d771dbe8f3da1818dd10
donguyenphu <donguyenphu022008@gmail.com>
2024-08-20 17:31:04 +0700

    no message

---
+++ JS_MAIN/profile.js
@@ -0,0 +1,36 @@
+console.log(123);
+
+
+/// email:
+/// name
+/// sdt 
+/// addd
+const email=document.getElementById('email');
+const name=document.getElementById('name');
+const AuthForm=document.getElementById('AuthForm');
+const phone=document.getElementById('phone');
+const address=document.getElementById('address')
+const formMessage=document.getElementById('formMessage');
+
+API.callWithToken().get('/auth/me').then(res => {
+    const data=res.data.data;
+    email.value=data.email;
+    name.value=data.name;
+    phone.value=data.phone;
+    address.value=data.address;
+}).catch(err => {
+    window.location.href='index.html';
+});
+
+AuthForm.addEventListener('submit', function(res) {
+    res.preventDefault();
+    const formDat=new FormData(AuthForm);
+    const data=Object.fromEntries(formDat);
+
+    API.callWithToken().put('/auth/update').then((res) => {
+        // formMessage.innerHTML=`<div class="alert alert-success" role="alert">
+        //     Cập nhật thông tin thành công
+        // </div>`;
+        toastMessage('SUCCESS UPDATE');
+    });
+});
